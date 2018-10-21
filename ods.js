6cbf4f492ed18b9602449fff052108f77e069e1d
timcsimmons <timothony@gmail.com>
2018-10-20 21:30:22 -0600

    Nest headers

--- ods.js
+++ ods.js
@@ -1,3 +1,4 @@
+// Combine counts with percentages
 function tabularTable(element) {
     var table = element.firstElementChild;
     var body = table.getElementsByTagName("tbody")[0];
@@ -7,7 +8,8 @@ function tabularTable(element) {
 
     var tr = document.createElement("tr");
     var th = document.createElement("th");
-    th.textContent = rows[0].firstElementChild.textContent.split("/")[0] + " [n (%)]";
+    th.textContent = rows[0].firstElementChild
+        .textContent.split("/")[0] + " [n (%)]";
     tr.appendChild(th);
     body.insertBefore(tr, body.firstElementChild);
 
@@ -36,6 +38,7 @@ function tabularTable(element) {
     return table;
 }
 
+// Combine means with SDs
 function tabularMean(element) {
     var table = element.firstElementChild;
     var body = table.getElementsByTagName("tbody")[0];
@@ -57,6 +60,7 @@ function tabularMean(element) {
     return table;
 }
 
+// Put the rows of one table into another and remove the first
 function tabularUnion(element1, element2) {
     var body1 = element1.querySelectorAll("tbody")[0];
     var body2 = element2.querySelectorAll("tbody")[0];
@@ -68,6 +72,7 @@ function tabularUnion(element1, element2) {
     element2.parentElement.removeChild(element2);
 }
 
+// Combine all tables in a tabulator section
 function tabularCombine(element) {
     var section = element.querySelectorAll("section");
     var t1 = section[0].querySelectorAll("table")[0];
@@ -88,7 +93,7 @@ function tabularCombine(element) {
     return t1;
 }
 
-
+// Restrict and re-order columns
 function tabularSelect(element, column) {
     var order = {};
     var head = element.querySelectorAll("thead")[0]
@@ -104,9 +109,10 @@ function tabularSelect(element, column) {
         head.appendChild(th);
         hth[loc].parentElement.removeChild(hth[loc]);
     }
+
     var body = element.querySelectorAll("tbody")[0];
     var row = body.querySelectorAll("tr");
-    console.log(order);
+
     for (var i = 0; i < row.length; i++) {
         var btd = row[i].querySelectorAll("td");
         for (var j = 0; j < column.length; j++) {
@@ -119,11 +125,69 @@ function tabularSelect(element, column) {
     }
 }
 
+// Nest the header columns in a table (factor1/level:factor2/level)
+function tabularNestHeader(element) {
+    var head = element.querySelectorAll("thead")[0];
+    var row = head.querySelectorAll("tr")[0];
+    var hth = row.querySelectorAll("th");
+
+    var colons = 0;
+    var i;
+    for (i = 0; i < hth.length; i++) {
+        var n = hth[i].textContent.split(":").length;
+        if (n > colons) colons = n;
+    }
+
+    for (i = 0; i < 2*colons; i++) {
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        tr.appendChild(td);
+
+        for (var j = 0; j < hth.length; j++) {
+            var text = hth[j].textContent.split(":");
+            var k = Math.floor(i/2);
+            var th = document.createElement("th");
+            if (k < text.length) {
+                th.textContent = text[k].split("/")[i % 2];
+            } else {
+                th.textContent = "";
+            }
+            tr.appendChild(th);
+        }
+        head.appendChild(tr);
+    }
+
+    row.parentElement.removeChild(row);
+    row = head.querySelectorAll("tr");
+    for (i = 0; i < row.length; i++) {
+        var rth = row[i].querySelectorAll("th");
+        var curn = 0;
+        var cur = rth[curn].textContents;
+        var colspan = 1;
+        for (j = 0; j < rth.length; j++) {
+            if (rth[j].textContent === cur && rth[j].textContent !== "") {
+                rth[j].parentElement.removeChild(rth[j]);
+                colspan++;
+                rth[curn].setAttribute("colspan", colspan);
+            } else {
+                colspan = 1;
+                curn = j;
+                cur = rth[curn].textContent;
+            }
+        }
+    }
+}
+
+// Test
 var tabular = document.getElementsByClassName("tabulator");
 var t0 = tabularCombine(tabular[0]);
+
 
 tabularSelect(t0, [
     "cyl/4:am/0", "cyl/4:am/1", "cyl/4",
     "cyl/6:am/0", "cyl/6:am/1", "cyl/6",
     "cyl/8:am/0", "cyl/8:am/1", "cyl/8",
     "(Intercept)"]);
+
+
+tabularNestHeader(t0);
